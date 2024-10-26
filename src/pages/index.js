27b6c291f7296e43351092574ec842fcc8b9c24a
Venky Donthula <venkateshdonthula2002@gmail.com>
2024-10-26 13:46:24 +0530

    addedd home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,72 @@
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
+import React from "react";
 
 
 export default function Home() {
+
+  const links = [
+    {
+      "name": "Voice Models",
+      "url": "/"
+    },
+    {
+      "name": "SDK",
+      "url": "/about"
+    },
+    {
+      "name": "About",
+      "url": "/contact"
+    },
+    {
+      "name": "Login",
+      "url": "/login"
+    }
+  ]
+
+
+
   return (
     <div className={styles.main}>
-      <h1 className={styles.title}>
+      <div className={styles.nav}>
+        <h2 className={styles.cname}>Recapture</h2>
+        <div className={styles.links}>
+          {links.map((e, index) => {
+            return (
+              <div
+                key={index}
+                className={styles.link_cont}
+              >
+                <Link href={e.url}
+                  className={styles.link}
+                >
+                  {e.name}
+                </Link>
+                {!(links.length == index + 1) && <div className={styles.bar} />}
+              </div>
+            )
+          })}
+          <Link href={"/"}>
+
+          </Link>
+
+        </div>
+      </div>
+      <div className={styles.base}>
+        <div className={styles.left}>
+
+        </div>
+        <div className={styles.right}>
+        </div>
+
+      </div>
+      {/* <h1 className={styles.title}>
         RECAPTURE
       </h1>
       <h2>
         Effortless Meeting Minutes!
       </h2>
-      <div className={styles.bar} />
+      <div className={styles.bar} /> */}
     </div>
   );
 }
